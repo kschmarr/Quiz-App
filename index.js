df7eeba7b8f1a7283620fa7461341764364f4b8e
Kris Schmarr <kschmarr@gmail.com>
2019-03-14 15:47:41 -0600

    fixed buggy last page

--- index.js
+++ index.js
@@ -15,26 +15,26 @@ function handleStart() {
   });
 }
 
-function generateOptions(){
+function generateOptions() {
   let optionsHTML = "";
-  for(let i=0;i<DEST[question].options.length;i++){
+  for (let i = 0; i < DEST[question].options.length; i++) {
     optionsHTML += `<div class="option">
-            <input type="radio" id="Option_${i+1}" name="option" value="${i+1}" required><span class="checkmark"></span>
-            <label for="Option_${i+1}">${DEST[question].options[i]}</label>
+            <input type="radio" id="Option_${i + 1}" name="option" value="${i + 1}" required><span class="checkmark"></span>
+            <label for="Option_${i + 1}">${DEST[question].options[i]}</label>
           </div>`
   }
   return optionsHTML;
 }
 
-function generateDestination(){
+function generateDestination() {
   return `<p>Select the location and rock type of:</p>
         <span>${DEST[question].name}</span>`
 }
 
-function handleSubmission(answer){
+function handleSubmission(answer) {
   console.log('`handleSubmission` ran');
   $('.go').text('Keep going!');
-  if(answer === DEST[question].answer){
+  if (answer === DEST[question].answer) {
     score++;
     $('.rightArea-text').html(`<p>You latched it!</p><p><img src="${DEST[question].picture}" alt="A picture of ${DEST[question].name}"></p><p>${DEST[question].name} is located in ${DEST[question].location}. The rock type is ${DEST[question].rock}, and the primary climbing discipline is ${DEST[question].style}.<p>`);
     $('.rightArea').removeClass('incorrect');
@@ -52,15 +52,16 @@ function handleRightArea() {
   $('.rightArea').submit((event) => {
     console.log('`handleRightArea` ran');
     event.preventDefault();
-    if(question < DEST.length-2){
-      question++;
+    question++;
+    if (question < DEST.length - 1) {
+      // question++;
       toggleRightArea();
       handleQuestion();
-    } else if(question === DEST.length-2){
-      question++;
+    } else if (question === DEST.length - 1) {
+      // question++;
       toggleRightArea();
       $('.right').text('Clip the anchor');
-      // handleQuestion();
+      handleQuestion();
     } else {
       console.log('`handleFinish` ran');
       toggleRightArea();
@@ -75,18 +76,18 @@ function handleRightArea() {
   });
 }
 
-function toggleRightArea(){
+function toggleRightArea() {
   console.log('`toggleRightArea` ran');
   $('.answerArea').toggleClass('hidden');
   $('.questionArea').toggleClass('hidden');
   $('.rightArea').toggleClass('hidden');
 }
 
-function handleQuestion(){
+function handleQuestion() {
   console.log('`handleQuestion` ran');
   $('.answerArea-text').html(generateOptions());
   $('.questionArea').html(generateDestination());
-  $('.question').html(`${question+1}`);
+  $('.question').html(`${question + 1}`);
   $('.score').html(`${score}`);
 }
 
@@ -97,11 +98,11 @@ function handleFinish() {
   });
 }
 
-function generateFinishText(){
+function generateFinishText() {
   let judgement = '';
-  if(score === 10){
+  if (score === 10) {
     judgement = 'You sent! Congrats!';
-  } else if(score === 0){
+  } else if (score === 0) {
     judgement = "You didn't leave the ground. You must be new to this.";
   } else {
     judgement = "You hung a few times. Train harder!"
